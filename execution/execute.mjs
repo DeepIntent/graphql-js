164cc0cbb2a1fb176b9ab7508079ed21c03b5588
chinmay1994 <chinu.k210@gmail.com>
2021-01-28 09:51:22 +0530

    Deploy c3eb92ad3369120a20aa53a12bcb2136290fbd19 to NPM branch

--- execution/execute.mjs
+++ execution/execute.mjs
@@ -507,7 +507,7 @@ function handleFieldError(rawError, fieldNodes, path, returnType, exeContext) {
   return null;
 }
 
-var queryExclusionList = ['locationsFlat', 'iabCategoriesFlat', 'iabCategoriesFlatV2', 'flatSafetySegments', 'devicesFlat', 'searchFlatSegments', 'devices', 'locations', 'flatSegmentsV2'];
+var queryExclusionList = ['locationsFlat', 'iabCategoriesFlat', 'iabCategoriesFlatV2', 'flatSafetySegments', 'devicesFlat', 'searchFlatSegments', 'devices', 'locations', 'flatSegmentsV2', 'campaignsV2'];
 /**
  * Implements the instructions for completeValue as defined in the
  * "Field entries" section of the spec.
